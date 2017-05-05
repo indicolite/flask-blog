3c7d438f8e12fa77624902ff21b4317f474f4ec6
indicolite <yuchao1219@gmail.com>
2017-05-05 08:44:53 +0800

    add factory method

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from config import DevConfig
 import wtforms
 from admin_pages import admin
 
+from models import db
+from controllers import blog
+
+def create_app(object_name):
+    """Create the app instance via `Factory Method`"""
+    app = Flask(__name__)
+    app.config.from_object(object_name)
+    db.init_app(app)
+
+    @app.route('/')
+    def index():
+        # Redirect the request_url '/' to '/blog/'
+        return redirect(url_for('blog.home'))
+
+    app.register_blueprint(blog.blog_blueprint)
+
+    return app
+
 #app.register_blueprint(admin)
-app = Flask(__name__)
+#app = Flask(__name__)
 # Import the views module
 #views = __import__('views')
 
@@ -12,16 +30,16 @@ app = Flask(__name__)
 # SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.
 # Set it to True or False to suppress this warning.
 # 'SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and '
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-app.config.from_object(DevConfig)
+#app.config.from_object(DevConfig)
 
 # When visiting http://server_ip/ GET(Default), call home()
 #@app.route('/')
 #def home():
 #    return '<h1>Hello World!</h1>'
 
-if __name__ == '__main__':
-    #Entry the application
-    app.register_blueprint(blog_blueprint)
-    app.run()
+#if __name__ == '__main__':
+#    #Entry the application
+#    app.register_blueprint(blog_blueprint)
+#    app.run()
