1b4b42617f63883849df54ea55e9eeba2d0c58dd
indicolite <yuchao1219@gmail.com>
2017-05-06 11:02:43 +0800

    add facebook oauth

--- flaskblog/controllers/main.py
+++ flaskblog/controllers/main.py
@@ -4,9 +4,11 @@
 from os import path
 from uuid import uuid4
 
-from flask import flash, url_for, redirect, render_template, Blueprint
+from flask import flash, url_for, redirect, render_template, Blueprint, request
 from flaskblog.forms import LoginForm, RegisterForm
 from flaskblog.models import db, User
+from flaskblog.extensions import openid, facebook
+
 
 main_blueprint = Blueprint(
         'main',
@@ -56,3 +58,37 @@ def register():
 
         return redirect(url_for('main.login'))
     return render_template('register.html', form=form)
+
+@main_blueprint.route('/facebook')
+def facebook_login():
+    return facebook.authorize(
+            callback=url_for('main.facebook_authorized',
+                next=request.referrer or None,
+                _external=True))
+
+@main_blueprint.route('/facebook/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+                request.args['error_reason'],
+                request.args['error_description'])
+    session['facebook_oauth_token'] = (resp['access_token'], '')
+
+    me = facebook.get('/me')
+
+    if me.data.get('first_name', False):
+        facebook_username = me.data['first_name'] + " " + me.data['last_name']
+    else:
+        facebook_username = me.data['name']
+
+    user = User.query.filter_by(username=facebook_username).first()
+    if user is None:
+        user = User(id=str(uuid4()), username=facebook_username, password='password')
+        db.session.add(user)
+        db.session.commit()
+
+    flash('You have been logged in.', category='success')
+
+    return redirect(url_for('blog.home'))
+
--- flaskblog/extensions.py
+++ flaskblog/extensions.py
@@ -2,6 +2,25 @@
 # coding=utf-8
 
 from flask_bcrypt import Bcrypt
+from flask_oauth import OAuth
+from flask_openid import OpenID as openid
 
 # Create the Flask-Bcrypt's instance
 bcrypt = Bcrypt()
+oauth = OAuth()
+
+# Create the auth object for Facebook
+facebook = oauth.remote_app(
+        'facebook',
+        base_url='https://graph.facebook.com/',
+        request_token_url=None,
+        access_token_url='/oauth/access_token',
+        authorize_url='https://www.facebook.com/dialog/oauth',
+        consumer_key='230064817479034',
+        consumer_secret='040715eb76405c55638e31d750d7ccff',
+        request_token_params={'scope': 'email'})
+
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_oauth_token')
+
